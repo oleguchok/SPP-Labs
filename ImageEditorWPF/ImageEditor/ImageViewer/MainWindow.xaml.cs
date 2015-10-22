8774436f82514c16df2aa618642231b0ff0f8787
oleguchok <oleguchok@gmail.com>
2015-10-23 00:20:22 +0300

    Add dockpanel with toolbar

--- ImageEditorWPF/ImageEditor/ImageViewer/MainWindow.xaml.cs
+++ ImageEditorWPF/ImageEditor/ImageViewer/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Win32;
+﻿using System.Windows.Forms;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+using SaveFileDialog = Microsoft.Win32.SaveFileDialog;
+
+using ImageInViewer = ImageViewer.Model.Image; 
 
 namespace ImageViewer
 {
@@ -21,9 +26,14 @@ namespace ImageViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string OpenFolderPath { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            OpenFolderPath = Environment.GetFolderPath(
+                Environment.SpecialFolder.MyPictures);
+            DataContext = this;
         }
 
         private void btnLoad_Click(object sender, RoutedEventArgs e)
@@ -36,6 +46,49 @@ namespace ImageViewer
             if (op.ShowDialog() == true)
             {
                 imgPhoto.Source = new BitmapImage(new Uri(op.FileName));
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sp = new SaveFileDialog();
+            sp.Title = "Save a picture";
+            sp.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (sp.ShowDialog() == true)
+            {
+                Image image = imgPhoto;
+                
+                
+            }
+        }
+
+        private void ButtonOpenFolder_OnClick(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+        }
+
+        public List<ImageInViewer> AllImages
+        {
+            get
+            {
+                List<ImageInViewer> result = new List<ImageInViewer>();
+                foreach (string filename in
+                    System.IO.Directory.GetFiles(OpenFolderPath))
+                {
+                    try
+                    {
+                        result.Add(
+                            new ImageInViewer(
+                            new BitmapImage(
+                            new Uri(filename)),
+                            System.IO.Path.GetFileNameWithoutExtension(filename)));
+                    }
+                    catch { }
+                }
+                return result;
             }
         }
     }
---
+++ ImageEditorWPF/ImageEditor/ImageViewer/Model/Image.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ImageViewer.Model
+{
+    public class Image
+    {
+        private ImageSource image;
+        private string name;
+
+        public Image(ImageSource image, string name)
+        {
+            this.image = image;
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+        public ImageSource ImageSrc
+        {
+            get { return image; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+
+    }
+}
