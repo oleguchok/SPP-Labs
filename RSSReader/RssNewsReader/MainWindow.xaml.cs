35601b482aac6285f2096e5d31dac243517d3d44
oleguchok <oleguchok@gmail.com>
2015-10-25 02:01:59 +0200

    Add rss feeds panel

--- RSSReader/RssNewsReader/MainWindow.xaml.cs
+++ RSSReader/RssNewsReader/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.ServiceModel.Syndication;
@@ -11,6 +13,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,9 +26,14 @@ namespace RssNewsReader
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private int menuShowClick = 0;
+        private ObservableCollection<string> rssFeedsList = new ObservableCollection<string>(
+                ConfigurationManager.AppSettings["rssFeedList"].Split(new char[] { ';' }));
+
+        public MainWindow() 
         {
             InitializeComponent();
+            RssFeedsList.DataContext = rssFeedsList;
         }
 
         private void OnGetFeed(object sender, RoutedEventArgs e)
@@ -43,6 +51,43 @@ namespace RssNewsReader
             catch (WebException ex)
             {
                 MessageBox.Show(ex.Message, "Syndication Reader");
+            }
+        }
+
+        private void btnTopMenuShow_Click(object sender, RoutedEventArgs e)
+        {
+            if (menuShowClick == 0)
+            {
+                ShowHideMenu("sbShowTopMenu", pnlTopMenu);
+                menuShowClick++;
+            }
+            else
+            {
+                menuShowClick = 0;
+                ShowHideMenu("sbHideTopMenu", pnlTopMenu);
+            }
+        }
+
+        private void ShowHideMenu(string Storyboard, StackPanel pnl)
+        {
+            Storyboard sb = Resources[Storyboard] as Storyboard;
+            sb.Begin(pnl);
+        }
+
+        private void AddNewRssFeedButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (NewRssFeedTextBox.Text != string.Empty)
+            {
+                rssFeedsList.Add(NewRssFeedTextBox.Text);
+                NewRssFeedTextBox.Text = string.Empty;
+            }
+        }
+
+        private void DeleteRssFeedButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (RssFeedsList.SelectedIndex != -1)
+            {
+                rssFeedsList.RemoveAt(RssFeedsList.SelectedIndex);
             }
         }
     }
