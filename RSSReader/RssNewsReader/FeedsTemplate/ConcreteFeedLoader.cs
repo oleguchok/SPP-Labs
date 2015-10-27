71478da0a36ecc783011bb86ca58bbbf5c23a64f
oleguchok <oleguchok@gmail.com>
2015-10-27 17:14:35 +0200

    Add task 5 ui

---
+++ RSSReader/RssNewsReader/FeedsTemplate/ConcreteFeedLoader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RssNewsReader.FeedsTemplate
+{
+    //public class ConcreteFeedLoader : FeedLoader
+    //{
+
+    //    protected override void SendToRecipients(IEnumerable<string> recipients, IEnumerable<System.ServiceModel.Syndication.SyndicationItem> feeds)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    protected override Func<System.ServiceModel.Syndication.SyndicationItem, System.ServiceModel.Syndication.SyndicationItem> FilterFeeds(IEnumerable<string> filterTags)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    protected override IEnumerable<System.ServiceModel.Syndication.SyndicationItem> LoadFeeds(IEnumerable<string> feedsToLoad)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
--- RSSReader/RssNewsReader/MainWindow.xaml.cs
+++ RSSReader/RssNewsReader/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.ServiceModel.Syndication;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,6 +19,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
+using RssNewsReader.FeedsTemplate;
 
 namespace RssNewsReader
 {
@@ -27,16 +29,24 @@ namespace RssNewsReader
     public partial class MainWindow : Window
     {
         private int menuShowClick = 0;
+        private TimeSpan checkedRadioButtonTimeGroup = new TimeSpan(0,1,0);
+        private List<SyndicationItem> currentFeeds = new List<SyndicationItem>(); 
+        private List<SyndicationItem> filterResult = new List<SyndicationItem>();
+
+        #region Observable Collections
         private ObservableCollection<string> rssFeedsList = new ObservableCollection<string>(
                 ConfigurationManager.AppSettings["rssFeedList"].Split(new char[] { ';' }));
         private ObservableCollection<string> emailList = new ObservableCollection<string>(
             ConfigurationManager.AppSettings["emailList"].Split(new char[] { ';' })); 
+        private ObservableCollection<string> tagList = new ObservableCollection<string>();
+        #endregion
 
         public MainWindow() 
         {
             InitializeComponent();
             RssFeedsList.DataContext = rssFeedsList;
             EmailList.DataContext = emailList;
+            TagList.DataContext = tagList;
         }
 
         private void OnGetFeed(object sender, RoutedEventArgs e)
@@ -56,6 +66,8 @@ namespace RssNewsReader
                 MessageBox.Show(ex.Message, "Syndication Reader");
             }
         }
+
+        #region Menu
 
         private void btnTopMenuShow_Click(object sender, RoutedEventArgs e)
         {
@@ -77,6 +89,10 @@ namespace RssNewsReader
             sb.Begin(pnl);
         }
 
+        #endregion
+
+        #region Menu ListBoxes
+
         private void AddNewRssFeedButton_OnClick(object sender, RoutedEventArgs e)
         {
             if (NewRssFeedTextBox.Text != string.Empty)
@@ -88,10 +104,7 @@ namespace RssNewsReader
 
         private void DeleteRssFeedButton_OnClick(object sender, RoutedEventArgs e)
         {
-            if (RssFeedsList.SelectedIndex != -1)
-            {
-                rssFeedsList.RemoveAt(RssFeedsList.SelectedIndex);
-            }
+            DeleteSelectedItemFromListBox(RssFeedsList.SelectedIndex, rssFeedsList);
         }
 
         private void AddEmailButton_OnClick(object sender, RoutedEventArgs e)
@@ -105,11 +118,91 @@ namespace RssNewsReader
 
         private void DeleteEmailButton_OnClick(object sender, RoutedEventArgs e)
         {
-            if (EmailList.SelectedIndex != -1)
+            DeleteSelectedItemFromListBox(EmailList.SelectedIndex, emailList);
+        }
+
+        private void DeleteTagButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            DeleteSelectedItemFromListBox(TagList.SelectedIndex, tagList);
+        }
+
+        private void AddTagButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (TagTextBox.Text != string.Empty)
             {
-                emailList.RemoveAt(EmailList.SelectedIndex);
+                tagList.Add(TagTextBox.Text);
+                TagTextBox.Text = string.Empty;
             }
         }
 
+        private void DeleteSelectedItemFromListBox(int itemNum, 
+            ObservableCollection<string> list)
+        {
+            if (itemNum != -1)
+                list.RemoveAt(itemNum);
+        }
+
+        #endregion
+
+        private void TimeRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            var radio = sender as RadioButton;
+            checkedRadioButtonTimeGroup = TimeSpan.Parse(radio.Tag.ToString());
+        }
+
+        private void GetFeedByCriteriasButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(GetFeedByTime, checkedRadioButtonTimeGroup);
+            ThreadPool.QueueUserWorkItem(ParseFeedByCtireria);
+           // ThreadPool.QueueUserWorkItem(loader.LoadFeedsEntry)
+            //FeedLoader loader = new ConcreteFeedLoader();
+        }
+
+        private void ParseFeedByCtireria(object state)
+        {
+            filterResult.Clear();
+            foreach (var tag in tagList)
+            {
+                lock (currentFeeds)
+                {
+                    foreach (var syndicationItem in currentFeeds)
+                    {
+                        if (syndicationItem.Summary.Text.Contains(tag))
+                        {
+                            filterResult.Add(syndicationItem);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void GetFeedByTime(object time)
+        {
+            lock (currentFeeds)
+            {
+                foreach (var feed in rssFeedsList)
+                {
+                    currentFeeds.AddRange(GetFeedsFromUrl(feed));
+                }
+            }
+        }
+
+        private IEnumerable<SyndicationItem> GetFeedsFromUrl(string feed)
+        {
+            try
+            {
+                using (XmlReader reader = XmlReader.Create(feed))
+                {
+                    var formatter = new Rss20FeedFormatter();
+                    formatter.ReadFrom(reader);
+                    return formatter.Feed.Items;
+                }
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show(ex.Message, "Syndication Reader");
+                return null;
+            }
+        }
     }
 }
