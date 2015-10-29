e23ba82b0ce3468c05e59d0eb3cf32df8b1f976c
oleguchok <oleguchok@gmail.com>
2015-10-29 18:17:48 +0200

    Init feed service and feed service host

---
+++ RSSReader/FeedService/IFeedService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Syndication;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FeedService
+{
+    [ServiceContract]
+    public interface IFeedService
+    {
+        [OperationContract]
+        Rss20FeedFormatter GetFeed(string feedUrl);
+    }
+
+}
